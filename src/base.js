a396bd6507cb825cc2533f4e5ac959c389b5d73e
DARYL LIVIO <58260833+Daryl97190@users.noreply.github.com>
2020-08-31 12:05:57 +0200

    Update base.js

--- src/base.js
+++ src/base.js
@@ -4,13 +4,13 @@ import "firebase/database";
 
 
 const config = {
-    apiKey: "AIzaSyAamA9YI7mihzuuM_Vj3Hlhnw1NiOPVA6Q",
-    authDomain: "chatbox-app-a34a7.firebaseapp.com",
-    databaseURL: "https://chatbox-app-a34a7.firebaseio.com",
-    projectId: "chatbox-app-a34a7",
-    storageBucket: "chatbox-app-a34a7.appspot.com",
-    messagingSenderId: "1088885232088",
-    appId: "1:1088885232088:web:a5e84f4e86003bb81322ad"
+    apiKey: "xxxxxx",
+    authDomain: "xxxxxx",
+    databaseURL: "xxxxxx",
+    projectId: "xxxxxx",
+    storageBucket: "xxxxxx",
+    messagingSenderId: "xxxxxx",
+    appId: "xxxxxx"
   };
   const firebaseApp = firebase.initializeApp(config);
   const base = Rebase.createClass(firebaseApp.database());
